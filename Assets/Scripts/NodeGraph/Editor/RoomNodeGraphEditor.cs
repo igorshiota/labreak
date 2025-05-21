3d0dfe290dd52da4ab3778968d3d1185e81edc71
Igor <igor.shiota@gmail.com>
2025-05-21 16:46:26 +0100

    Initial commit, building a room dungeon editor

---
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEditor.Callbacks;
+using UnityEditor;
+
+public class RoomNodeGraphEditor : EditorWindow
+{
+    private GUIStyle roomNodeStyle;
+    private static RoomNodeGraphSO currentRoomNodeGraph;
+
+    //Node layout values
+    private const float nodeWidth = 160f;
+    private const float nodeHeight = 75f;
+    private const int nodePadding = 25;
+    private const int nodeBorder = 12;
+
+    [MenuItem("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
+
+    private static void OpenWindow()
+    {
+        GetWindow<RoomNodeGraphEditor>("Room Node Graph Editor");
+    }
+
+    private void OnEnable()
+    {
+        //Define node layout style
+        roomNodeStyle = new GUIStyle();
+        roomNodeStyle.normal.background = EditorGUIUtility.Load("node1") as Texture2D;
+        roomNodeStyle.normal.textColor = Color.white;
+        roomNodeStyle.padding = new RectOffset(nodePadding, nodePadding, nodePadding, nodePadding);
+        roomNodeStyle.border = new RectOffset(nodeBorder, nodeBorder, nodeBorder, nodeBorder);
+    }
+
+    /// <summary>
+    /// Open the room node graph editor window if a room node graph scriptable object asset is double clicked in the inspector
+    /// </summary>
+
+    [OnOpenAsset(0)] // Need the namespace UnityEditor.Callbacks
+
+    public static bool OnDoubleClickAsset(int instanceID, int line)
+    {
+        RoomNodeGraphSO roomNodeGraph = EditorUtility.InstanceIDToObject(instanceID) as RoomNodeGraphSO;
+
+        if (roomNodeGraph != null)
+        {
+            OpenWindow();
+
+            currentRoomNodeGraph = roomNodeGraph;
+
+            return true;
+        }
+        return false;
+    }
+
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(new Vector2(100f, 100f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
+        EditorGUILayout.LabelField("Node 1");
+        GUILayout.EndArea();
+
+        GUILayout.BeginArea(new Rect(new Vector2(300f, 300f), new Vector2(nodeWidth, nodeHeight)), roomNodeStyle);
+        EditorGUILayout.LabelField("Node 2");
+        GUILayout.EndArea();
+    }
+}
